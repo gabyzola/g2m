be2a6d26dfc0801fb46e84c752d406a1bd5c5012
gabyzola <zolag@merrimack.edu>
2025-11-14 14:37:07 -0500

    Remove HTTPS configuration from vite.config.js and simplify server setup

--- MackAdapt UI/vite.config.js
+++ MackAdapt UI/vite.config.js
@@ -1,14 +1,9 @@
 import { defineConfig } from 'vite';
-import fs from 'fs';
 
 export default defineConfig({
   server: {
     host: true,
     allowedHosts: true,
     port: 49160,
-    https: {
-      key: fs.readFileSync('./certs/privkey.pem'),
-      cert: fs.readFileSync('./certs/fullchain.pem')
-    }
   }
 });
