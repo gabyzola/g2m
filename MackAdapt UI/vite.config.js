b8fb25c56f7de193b7a284ff768f5ce00f9f333b
mikaelapom <pomfretm@merrimack.edu>
2025-12-18 21:23:38 -0500

    final fixes

--- MackAdapt UI/vite.config.js
+++ MackAdapt UI/vite.config.js
@@ -8,7 +8,12 @@ export default defineConfig({
         main: path.resolve(__dirname, "index.html"),
         login: path.resolve(__dirname, "login.html"),
         classes: path.resolve(__dirname, "classes.html"),
-        "student-dashboard": path.resolve(__dirname, "student-dashboard.html")
+        "student-dashboard": path.resolve(__dirname, "student-dashboard.html"),
+        quiz: path.resolve(__dirname, "quiz.html"),          
+        "class-enroll": path.resolve(__dirname, "class-enroll.html"),
+        "quiz-create": path.resolve(__dirname, "quiz-create.html"),
+        "class-module": path.resolve(__dirname, "class-module.html"),
+        "results": path.resolve(__dirname, "results.html")
       }
     },
     outDir: "dist"
