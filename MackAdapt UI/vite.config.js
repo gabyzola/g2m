ff4b54d1ecdadc93a6304b81c156225401d692f6
mikaelapom <pomfretm@merrimack.edu>
2025-12-18 21:07:43 -0500

    another fix

--- MackAdapt UI/vite.config.js
+++ MackAdapt UI/vite.config.js
@@ -2,6 +2,17 @@ import { defineConfig } from 'vite';
 import path from 'path'
 
 export default defineConfig({
+  build: {
+    rollupOptions: {
+      input: {
+        main: path.resolve(__dirname, "index.html"),
+        login: path.resolve(__dirname, "login.html"),
+        classes: path.resolve(__dirname, "classes.html"),
+        "student-dashboard": path.resolve(__dirname, "student-dashboard.html")
+      }
+    },
+    outDir: "dist"
+  },
   server: {
     host: true,
     allowedHosts: true,
